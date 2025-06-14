2c6132da7af246106c305ee20b2ccaae91d17621
Csaba <33700462+csaba-nagy@users.noreply.github.com>
2025-06-14 11:41:04 +0200

    feat: add the basic sequential solution

--- cmd/psps/main.go
+++ cmd/psps/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("🐱")
+	for i := 1; i <= 10000; i++ {
+		conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", i))
+		if err != nil {
+			log.Printf("%d CLOSED (%s)\n", i, err)
+
+			continue
+		}
+
+		log.Printf("%d OPEN\n", i)
+
+		conn.Close()
+	}
+
+	log.Println("DONE")
 }
