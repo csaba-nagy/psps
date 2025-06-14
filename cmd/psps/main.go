52c70bedb9402a952751c90471634e5ba312c391
Csaba <33700462+csaba-nagy@users.noreply.github.com>
2025-06-14 12:40:24 +0200

    feat: add basic concurrency
    I use Waitgroup from the sync package of the standard library

--- cmd/psps/main.go
+++ cmd/psps/main.go
@@ -1,24 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"sync"
 )
 
+var host string
+var fromPort string
+var toPort string
+
+func init() {
+	flag.StringVar(&host, "host", "127.0.01", "Host to scan")
+	flag.StringVar(&fromPort, "from", "8080", "Port to start scanning from")
+	flag.StringVar(&toPort, "to", "8090", "Port at which to stop scanning")
+}
+
 func main() {
-	for i := 1; i <= 10000; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", i))
-		if err != nil {
-			log.Printf("%d CLOSED (%s)\n", i, err)
+	flag.Parse()
 
-			continue
-		}
-
-		log.Printf("%d OPEN\n", i)
-
-		conn.Close()
+	fp, err := strconv.Atoi(fromPort)
+	if err != nil {
+		log.Fatalf("Invalid value at 'from': %s", fromPort)
 	}
+
+	tp, err := strconv.Atoi(toPort)
+	if err != nil {
+		log.Fatalf("Invalid value at 'to': %s", toPort)
+	}
+
+	if fp > tp {
+		log.Fatalln("Invalid values for 'from' and 'to' port: from value cannot be higher than to")
+	}
+
+	var wg sync.WaitGroup
+	goroutinesCountToWaitOn := tp - fp + 1
+
+	wg.Add(goroutinesCountToWaitOn)
+
+	for i := fp; i <= tp; i++ {
+		go func(p int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, p))
+			if err != nil {
+				log.Printf("%d CLOSED (%s)\n", p, err)
+
+				return
+			}
+
+			log.Printf("%d OPEN\n", p)
+
+			conn.Close()
+		}(i)
+	}
+
+	wg.Wait()
 
 	log.Println("DONE")
 }
