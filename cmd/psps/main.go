7201d10372b4b2054f5fcdf3202962f4b4675ffc
Csaba <33700462+csaba-nagy@users.noreply.github.com>
2025-06-14 16:16:29 +0200

    feat: introduce the worker pattern

--- cmd/psps/main.go
+++ cmd/psps/main.go
@@ -3,62 +3,103 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net"
-	"strconv"
-	"sync"
+	"os"
+	"os/signal"
+	"runtime"
+	"sort"
+	"syscall"
 )
 
-var host string
-var fromPort string
-var toPort string
+var (
+	host         string
+	fromPort     int
+	toPort       int
+	numOfWorkers int
+)
 
 func init() {
 	flag.StringVar(&host, "host", "127.0.01", "Host to scan")
-	flag.StringVar(&fromPort, "from", "8080", "Port to start scanning from")
-	flag.StringVar(&toPort, "to", "8090", "Port at which to stop scanning")
+	flag.IntVar(&fromPort, "from", 8080, "Port to start scanning from")
+	flag.IntVar(&toPort, "to", 8090, "Port at which to stop scanning")
+	flag.IntVar(&numOfWorkers, "workers", runtime.NumCPU(), "Number of the workers. Defaults to system's number of CPUs.")
 }
 
 func main() {
 	flag.Parse()
 
-	fp, err := strconv.Atoi(fromPort)
-	if err != nil {
-		log.Fatalf("Invalid value at 'from': %s", fromPort)
+	var openPorts []int
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		printResults(openPorts)
+
+		os.Exit(0)
+	}()
+
+	portsToScan := getPortListToScan(fromPort, toPort)
+
+	portsChan := make(chan int, numOfWorkers)
+	resultsChan := make(chan int)
+
+	for range cap(portsChan) {
+		go worker(host, portsChan, resultsChan)
 	}
 
-	tp, err := strconv.Atoi(toPort)
-	if err != nil {
-		log.Fatalf("Invalid value at 'to': %s", toPort)
+	go func() {
+		for _, p := range portsToScan {
+			portsChan <- p
+		}
+	}()
+
+	for range len(portsToScan) {
+		if p := <-resultsChan; p != 0 {
+			openPorts = append(openPorts, p)
+		}
 	}
 
-	if fp > tp {
-		log.Fatalln("Invalid values for 'from' and 'to' port: from value cannot be higher than to")
+	close(portsChan)
+	close(resultsChan)
+
+	printResults(openPorts)
+}
+
+func worker(host string, portsChan <-chan int, resultsChan chan<- int) {
+	for p := range portsChan {
+		address := fmt.Sprintf("%s:%d", host, p)
+
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			resultsChan <- 0
+
+			continue
+		}
+
+		conn.Close()
+		resultsChan <- p
+	}
+}
+
+func printResults(ports []int) {
+	sort.Ints(ports)
+
+	fmt.Println("📊 REPORT")
+	fmt.Println("🚪Opened ports")
+
+	for _, p := range ports {
+		fmt.Printf("%d\n", p)
+	}
+}
+
+func getPortListToScan(fromPort, toPort int) []int {
+	list := make([]int, 0, toPort-fromPort+1)
+
+	for i := fromPort; i <= toPort; i++ {
+		list = append(list, i)
 	}
 
-	var wg sync.WaitGroup
-	goroutinesCountToWaitOn := tp - fp + 1
-
-	wg.Add(goroutinesCountToWaitOn)
-
-	for i := fp; i <= tp; i++ {
-		go func(p int) {
-			defer wg.Done()
-
-			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, p))
-			if err != nil {
-				log.Printf("%d CLOSED (%s)\n", p, err)
-
-				return
-			}
-
-			log.Printf("%d OPEN\n", p)
-
-			conn.Close()
-		}(i)
-	}
-
-	wg.Wait()
-
-	log.Println("DONE")
+	return list
 }
